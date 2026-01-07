cecd23e95a9d88c2c879645ed2dfe6f30d8ab0f3
marfa <itsnglinh@gmail.com>
2026-01-07 13:20:10 +1100

    chore: Add allowedHosts to vite.config.js for easier deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: 'all'
+  },
   resolve: {
     alias:  {
       '@': path.resolve(__dirname, './src'),
