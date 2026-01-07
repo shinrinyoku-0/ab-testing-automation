87dbc034fd15719901b9d2443e14fa1befd5d871
marfa <itsnglinh@gmail.com>
2026-01-07 13:10:30 +1100

    Chore: Change Dockerfile and init_db.sh for easier deployment

--- src/init_db.sh
+++ src/init_db.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-echo "Waiting for PostgreSQL to be ready..."
-while ! pg_isready -h db -p 5432 -U postgres; do
-  sleep 1
-done
-
 echo "Running Alembic migrations..."
 alembic upgrade head
-
 echo "Database initialized!"
 exec "$@"
